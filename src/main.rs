c297b0973df38bbf30db5253565c9c1c9e7a23b2
Akihito Kirisaki <kirisaki@klara.works>
2024-05-18 18:49:36 +0900

    feat: send messages

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use std::time::{Duration, Instant};
+use std::{
+    collections::HashMap,
+    sync::{atomic::AtomicUsize, Arc},
+    time::{Duration, Instant},
+};
 
 use actix::prelude::*;
 use actix_files::NamedFile;
 use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web_actors::ws;
+use rand::{rngs::ThreadRng, Rng};
 
 async fn index() -> impl Responder {
     NamedFile::open_async("./static/index.html").await.unwrap()
@@ -11,36 +16,87 @@ async fn index() -> impl Responder {
 
 const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
 
-/// How long before lack of client response causes a timeout
 const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 
-/// websocket connection is long running connection, it easier
-/// to handle with an actor
-pub struct MyWebSocket {
-    /// Client must send ping at least once per 10 seconds (CLIENT_TIMEOUT),
-    /// otherwise we drop connection.
-    hb: Instant,
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Message(pub String);
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Topple;
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Setup;
+
+#[derive(Message)]
+#[rtype(usize)]
+pub struct Connect {
+    pub addr: Recipient<Message>,
 }
 
-impl MyWebSocket {
-    pub fn new() -> Self {
-        Self { hb: Instant::now() }
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Disconnect {
+    pub id: usize,
+}
+
+pub struct DominoServer {
+    sessions: HashMap<usize, Recipient<Message>>,
+    rng: ThreadRng,
+    domino_count: Arc<AtomicUsize>,
+}
+
+impl DominoServer {
+    pub fn new(domino_count: Arc<AtomicUsize>) -> Self {
+        DominoServer {
+            sessions: HashMap::new(),
+            rng: rand::thread_rng(),
+            domino_count,
+        }
     }
+}
 
-    /// helper method that sends ping to client every 5 seconds (HEARTBEAT_INTERVAL).
-    ///
-    /// also this method checks heartbeats from client
-    fn hb(&self, ctx: &mut <Self as Actor>::Context) {
+impl Actor for DominoServer {
+    type Context = Context<Self>;
+}
+
+impl Handler<Connect> for DominoServer {
+    type Result = usize;
+
+    fn handle(&mut self, msg: Connect, _: &mut Context<Self>) -> Self::Result {
+        println!("Someone joined");
+
+        let id = self.rng.gen::<usize>();
+        self.sessions.insert(id, msg.addr);
+
+        id
+    }
+}
+
+impl Handler<Disconnect> for DominoServer {
+    type Result = ();
+
+    fn handle(&mut self, msg: Disconnect, _: &mut Context<Self>) {
+        println!("Someone disconnected");
+
+        self.sessions.remove(&msg.id);
+    }
+}
+
+pub struct WsSession {
+    pub id: usize,
+    pub hb: Instant,
+    pub addr: Addr<DominoServer>,
+}
+
+impl WsSession {
+    fn hb(&self, ctx: &mut ws::WebsocketContext<Self>) {
         ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
-            // check client heartbeats
             if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
-                // heartbeat timed out
                 println!("Websocket Client heartbeat failed, disconnecting!");
-
-                // stop actor
                 ctx.stop();
-
-                // don't try to send a ping
                 return;
             }
 
@@ -49,17 +105,15 @@ impl MyWebSocket {
     }
 }
 
-impl Actor for MyWebSocket {
+impl Actor for WsSession {
     type Context = ws::WebsocketContext<Self>;
 
-    /// Method is called on actor start. We start the heartbeat process here.
     fn started(&mut self, ctx: &mut Self::Context) {
         self.hb(ctx);
     }
 }
 
-/// Handler for `ws::Message`
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWebSocket {
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsSession {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         // process websocket messages
         println!("WS: {msg:?}");
@@ -71,7 +125,13 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWebSocket {
             Ok(ws::Message::Pong(_)) => {
                 self.hb = Instant::now();
             }
-            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Text(text)) => {
+                if text.starts_with("setup") {
+                    ctx.text("s");
+                } else if text.starts_with("topple") {
+                    ctx.text("t");
+                }
+            }
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
             Ok(ws::Message::Close(reason)) => {
                 ctx.close(reason);
@@ -82,18 +142,36 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWebSocket {
     }
 }
 
-async fn echo_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    ws::start(MyWebSocket::new(), &req, stream)
+async fn domino_route(
+    req: HttpRequest,
+    stream: web::Payload,
+    srv: web::Data<Addr<DominoServer>>,
+) -> Result<HttpResponse, Error> {
+    ws::start(
+        WsSession {
+            id: 0,
+            hb: Instant::now(),
+            addr: srv.get_ref().clone(),
+        },
+        &req,
+        stream,
+    )
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     log::info!("starting HTTP server at http://localhost:8080");
-    HttpServer::new(|| {
+
+    let app_state = Arc::new(AtomicUsize::new(0));
+    let server = DominoServer::new(app_state.clone()).start();
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::from(app_state.clone()))
+            .app_data(web::Data::new(server.clone()))
             .service(web::resource("/").to(index))
-            .service(web::resource("/ws").route(web::get().to(echo_ws)))
+            .service(web::resource("/ws").route(web::get().to(domino_route)))
             .wrap(middleware::Logger::default())
     })
     .workers(2)
