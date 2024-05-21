9428e9a269133f87f49f82387bd4fffe185526d3
Akihito Kirisaki <kirisaki@klara.works>
2024-05-21 11:03:07 +0900

    feat: topple domino

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::HashMap,
+    sync::atomic::Ordering,
     sync::{atomic::AtomicUsize, Arc},
     time::{Duration, Instant},
 };
@@ -9,6 +10,7 @@ use actix_files::NamedFile;
 use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web_actors::ws;
 use rand::{rngs::ThreadRng, Rng};
+use serde::Serialize;
 
 async fn index() -> impl Responder {
     NamedFile::open_async("./static/index.html").await.unwrap()
@@ -23,11 +25,11 @@ const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 pub struct Message(pub String);
 
 #[derive(Message)]
-#[rtype(result = "()")]
+#[rtype(Domino)]
 pub struct Topple;
 
 #[derive(Message)]
-#[rtype(result = "()")]
+#[rtype(Domino)]
 pub struct Setup;
 
 #[derive(Message)]
@@ -42,18 +44,26 @@ pub struct Disconnect {
     pub id: usize,
 }
 
+#[derive(Debug, Clone, Serialize, MessageResponse)]
+pub struct Domino {
+    domino: usize,
+    high: usize,
+}
+
 pub struct DominoServer {
     sessions: HashMap<usize, Recipient<Message>>,
     rng: ThreadRng,
     domino_count: Arc<AtomicUsize>,
+    high: Arc<AtomicUsize>,
 }
 
 impl DominoServer {
-    pub fn new(domino_count: Arc<AtomicUsize>) -> Self {
+    pub fn new(domino_count: Arc<AtomicUsize>, high: Arc<AtomicUsize>) -> Self {
         DominoServer {
             sessions: HashMap::new(),
             rng: rand::thread_rng(),
             domino_count,
+            high,
         }
     }
 }
@@ -82,6 +92,33 @@ impl Handler<Disconnect> for DominoServer {
         println!("Someone disconnected");
 
         self.sessions.remove(&msg.id);
+    }
+}
+
+impl Handler<Setup> for DominoServer {
+    type Result = Domino;
+
+    fn handle(&mut self, _msg: Setup, _ctx: &mut Self::Context) -> Self::Result {
+        let domino = self.domino_count.fetch_add(1, Ordering::SeqCst) + 1;
+        let mut high = self.high.load(Ordering::SeqCst);
+        if high < domino {
+            self.high.store(domino, Ordering::SeqCst);
+            high = domino;
+        }
+
+        Domino { domino, high }
+    }
+}
+
+impl Handler<Topple> for DominoServer {
+    type Result = Domino;
+
+    fn handle(&mut self, _msg: Topple, _ctx: &mut Self::Context) -> Self::Result {
+        self.domino_count.store(0, Ordering::SeqCst);
+        Domino {
+            domino: self.domino_count.load(Ordering::SeqCst),
+            high: self.high.load(Ordering::SeqCst),
+        }
     }
 }
 
@@ -127,9 +164,23 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsSession {
             }
             Ok(ws::Message::Text(text)) => {
                 if text.starts_with("setup") {
-                    ctx.text("s");
+                    self.addr
+                        .send(Setup)
+                        .into_actor(self)
+                        .then(|res, _, ctx| {
+                            ctx.text(serde_json::to_string(&res.unwrap()).unwrap());
+                            fut::ready(())
+                        })
+                        .wait(ctx)
                 } else if text.starts_with("topple") {
-                    ctx.text("t");
+                    self.addr
+                        .send(Topple)
+                        .into_actor(self)
+                        .then(|res, _, ctx| {
+                            ctx.text(serde_json::to_string(&res.unwrap()).unwrap());
+                            fut::ready(())
+                        })
+                        .wait(ctx)
                 }
             }
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
@@ -163,12 +214,14 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     log::info!("starting HTTP server at http://localhost:8080");
 
-    let app_state = Arc::new(AtomicUsize::new(0));
-    let server = DominoServer::new(app_state.clone()).start();
+    let domino_count = Arc::new(AtomicUsize::new(0));
+    let high = Arc::new(AtomicUsize::new(0));
+    let server = DominoServer::new(domino_count.clone(), high.clone()).start();
 
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::from(app_state.clone()))
+            .app_data(web::Data::from(domino_count.clone()))
+            .app_data(web::Data::from(high.clone()))
             .app_data(web::Data::new(server.clone()))
             .service(web::resource("/").to(index))
             .service(web::resource("/ws").route(web::get().to(domino_route)))
